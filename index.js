1a4f6ec4a38ebea719dad4bdc8e89b58eb928b6e
Karthikeyan Mariappan <karthikeyan.mariappan@bowbridge.net>
2021-05-19 13:34:45 +0200

    Test Notes

--- index.js
+++ index.js
@@ -1,15 +1,16 @@
 let a = 10
 
-let b = 20 
+let b = 20
 
-console.log(a+b)
+console.log(a + b)
 console.log(`${a}${b}`)
 
-let x= 11
+let x = 11
 
-if(x===10){
-    console.log('X value id 10')
+if (x === 10) {
+  console.log('X value id 10')
 } else if (x > 10) {
-    console.log('X Value is morethan 10')
+  console.log('X Value is morethan 10')
 }
 
+// KArthik notes
