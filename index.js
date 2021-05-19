3a953c6f147bdc0e18f60c21db7bbd6714e9ae8a
Nirmal Kumar <nirmal31.editor@gmail.com>
2021-05-19 11:30:25 +0530

    Code Test

---
+++ index.js
@@ -0,0 +1,15 @@
+let a = 10
+
+let b = 20 
+
+console.log(a+b)
+console.log(`${a}${b}`)
+
+let x= 11
+
+if(x===10){
+    console.log('X value id 10')
+} else if (x > 10) {
+    console.log('X Value is morethan 10')
+}
+
